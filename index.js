8eab9df73b9b01efb188fcba94d9ba4a4a0028d4
Melanie Williams <melaniep518@gmail.com>
2020-05-28 11:54:11 -0400

    Add zip code input and make API call based on input

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function loadDoc() {
+window.addEventListener('load', function() {
+  document.getElementById('zipCodeSubmit').onclick = getWeatherByZip;
+})
+
+function getWeatherByZip() {
+  const zipCode = document.getElementById('zipCodeInput').value;
+  const fiveDigits = /^\d{5}$/;
+  if(fiveDigits.test(zipCode)) {
+    loadWeather(zipCode)
+  }
+}
+
+function loadWeather(zipCode) {
   const key = 'a6a2cec7737de68bac44ec6349002f5b';
-  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${11221}&units=imperial&appid=${key}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${key}`;
 
   const xhttp = new XMLHttpRequest();
   
@@ -20,8 +32,3 @@ function loadDoc() {
 function displayWeather(weather) {
   document.getElementById('weather').innerHTML = weather;
 };
-
-
-loadDoc();
-
-
